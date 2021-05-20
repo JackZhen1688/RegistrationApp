674d0ae9c640a3a1061da5a146c26452df5257d6
Devon Zhen <dz89@nji.edu>
2021-05-20 11:54:40 -0400

    Add MockMvc Junit test

--- src/test/java/njit/cs/demo/junit/SpringBootApplicationJunitTest.java
+++ src/test/java/njit/cs/demo/junit/SpringBootApplicationJunitTest.java
@@ -1,15 +1,27 @@
 package njit.cs.demo.junit;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 //import org.junit.ComparisonFailure;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import njit.cs.demo.configuration.JNDIConfiguration;
 import njit.cs.demo.controller.PersonController;
 import njit.cs.demo.dto.PhoneTypeDTO;
 import njit.cs.demo.repository.PhoneTypeRepository;
@@ -17,12 +29,12 @@ import njit.cs.demo.service.PersonService;
 import njit.cs.demo.service.PersonTypeService;
 import njit.cs.demo.service.PhoneTypeService;
 
+@SuppressWarnings("deprecation")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {
-	      	    Application.class, 
-                JNDIConfigJunit.class})
+@SpringBootTest(classes = { Application.class, JNDIConfiguration.class })
 @ContextConfiguration(classes = {PersonController.class, PersonService.class, PhoneTypeService.class, PersonTypeService.class})  
                                                            // add due to no scan service in RegistrationApp
+@AutoConfigureMockMvc
 public class SpringBootApplicationJunitTest {
 
 	@Autowired
@@ -33,6 +45,17 @@ public class SpringBootApplicationJunitTest {
 	
 	@Autowired
 	PhoneTypeRepository phoneTypeRepository;               // yes scan repository in RegistrationApp
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception
+	{
+		DriverManagerDataSource dataSource = getDataSource();
+		SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+		builder.bind("jdbc/demoDS", dataSource);
+	}
 	
 	@Test
 	public void testPhoneTypeList() 
@@ -56,6 +79,52 @@ public class SpringBootApplicationJunitTest {
 			    System.out.println("=============================================");
 			}
 		}
+	}
+	
+	
+	
+	@Test
+	public void testMockPhoneTypeList() {
+		try {
+			MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+					                     .get("/phoneTypes")
+					                     .contentType("applicaation/json"))
+					                   //.content(""))
+					                     .andExpect(status().isOk()).andReturn();
+		    Assert.assertNotNull("Result is not null", mvcResult.getResponse());                   
+			String jsonValue =  mvcResult.getResponse().getContentAsString();
+			
+			System.out.println("String === "+jsonValue);
+		    System.out.println("=============================================");
+	        System.out.println("### PhoneTypeList() Successful: Return "+mvcResult.getResponse().getContentLength()+") ###");
+		    System.out.println("=============================================");
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	private static DriverManagerDataSource getDataSource() 
+	{
+		DriverManagerDataSource dataSource = null;
+		try {
+			Properties props = new Properties();
+			InputStream fileIn = SpringBootApplicationJunitTest.class.getClassLoader().getResourceAsStream("Registration.properties");
+			props.load(fileIn);
+			dataSource = new DriverManagerDataSource();
+	        dataSource.setDriverClassName(props.getProperty("driverClass"));
+	        dataSource.setUrl(props.getProperty("connURL"));
+	        dataSource.setUsername(props.getProperty("dbUsername"));
+	        dataSource.setPassword(props.getProperty("dbPassword"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return dataSource;
 	}
 	
 	/*
